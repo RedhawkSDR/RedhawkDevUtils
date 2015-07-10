04431bba41a2660ab78ffda992be6a2a24dfb0e3
Kipp Bowen <jkb@axiosengineering.com>
2015-07-10 14:25:54 -0400

    regen in RH 2.0, incremented major version, and added rh namespace

--- build.sh
+++ build.sh
@@ -1,52 +1,40 @@
-#!/bin/sh
-#
-# This file is protected by Copyright. Please refer to the COPYRIGHT file
-# distributed with this source distribution.
-#
-# This file is part of REDHAWK Basic Components RedhawkDevUtils.
-#
-# REDHAWK Basic Components RedhawkDevUtils is free software: you can redistribute it and/or modify it
-# under the terms of the GNU Lesser General Public License as published by the
-# Free Software Foundation, either version 3 of the License, or (at your
-# option) any later version.
-#
-# REDHAWK Basic Components RedhawkDevUtils is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
-# for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with this program.  If not, see http://www.gnu.org/licenses/.
-#
-config_ac='configure.ac'
-make_am='Makefile.am'
-makefile='Makefile'
+#!/bin/bash
 
-if [ "$1" == "rpm" ]; then
+if [ "$1" = "rpm" ]; then
     # A very simplistic RPM build scenario
-    if [ -e RedhawkDevUtils.spec ]; then
+    if [ -e rh.RedhawkDevUtils.spec ]; then
         mydir=`dirname $0`
         tmpdir=`mktemp -d`
-        cp -r ${mydir} ${tmpdir}/RedhawkDevUtils_v1-3.1.0
-        tar czf ${tmpdir}/RedhawkDevUtils_v1-3.1.0.tar.gz --exclude=".svn" -C ${tmpdir} RedhawkDevUtils_v1-3.1.0
-        rpmbuild -ta ${tmpdir}/RedhawkDevUtils_v1-3.1.0.tar.gz
+        cp -r ${mydir} ${tmpdir}/rh.RedhawkDevUtils-4.0.0
+        tar czf ${tmpdir}/rh.RedhawkDevUtils-4.0.0.tar.gz --exclude=".svn" -C ${tmpdir} rh.RedhawkDevUtils-4.0.0
+        rpmbuild -ta ${tmpdir}/rh.RedhawkDevUtils-4.0.0.tar.gz
         rm -rf $tmpdir
     else
         echo "Missing RPM spec file in" `pwd`
         exit 1
     fi
-elif [ -e build.sh ]; then
-    if [ "$1" == 'clean' ]; then
-            make clean
-    else
-    # Checks if build is newer than makefile (based on modification time)
-        if [[ $config_ac -nt $makefile || $make_am -nt $makefile ]]; then
-            ./reconf
-            ./configure
-        fi
-            make
-            exit 0
-    fi
 else
-    echo "No build.sh found for $impl"
+    for impl in cpp ; do
+        cd $impl
+        if [ -e build.sh ]; then
+            if [ $# == 1 ]; then
+                if [ $1 == 'clean' ]; then
+                    rm -f Makefile
+                    rm -f config.*
+                    ./build.sh distclean
+                else
+                    ./build.sh $*
+                fi
+            else
+                ./build.sh $*
+            fi
+        elif [ -e Makefile ] && [ Makefile.am -ot Makefile ]; then
+            make $*
+        elif [ -e reconf ]; then
+            ./reconf && ./configure && make $*
+        else
+            echo "No build.sh found for $impl"
+        fi
+        cd -
+    done
 fi
